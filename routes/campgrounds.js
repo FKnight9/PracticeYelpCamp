698be37e7507cbeb51518032c232e385a80441bb
Faris Ibrahim <faristheknight9@gmail.com>
2017-08-08 13:27:17 +0000

    Added authentication middlwear to campgrounds route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res){
 });
 
 //Create
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
@@ -34,7 +34,7 @@ router.post("/", function(req, res){
 });
 
 //New
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn,function(req, res){
    res.render("campgrounds/new"); 
 });
 
@@ -49,5 +49,13 @@ router.get("/:id", function(req,res){
         }
     });    
 });
+
+//middlewear
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
