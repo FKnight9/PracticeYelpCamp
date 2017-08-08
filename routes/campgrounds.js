1c373338aa09a6a43935314a338dd243d4dee959
Faris Ibrahim <faristheknight9@gmail.com>
2017-08-08 14:08:41 +0000

    Associate a campground with a user

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,10 +18,15 @@ router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var author ={
+        id: req.user._id,
+        username: req.user.username
+    };
     var newCampground = {
         name: name,
         image: image,
-        description: desc
+        description: desc,
+        author: author
     };
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
